07e9f44c9b0786d0226e7bb04c2f1bc653d8ddcc
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2023-09-20 14:11:59 -0400

    Optimisation du formulaire pour Item

--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -1,21 +1,16 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-
 import { Observable } from "rxjs";
-import { catchError, tap } from "rxjs/operators";
-
+import { catchError } from "rxjs/operators";
 import { ErrorHandlerService } from "./error-handler.service";
-
-import {Pret} from "../models/Pret";
-
 
 @Injectable({
   providedIn: "root",
 })
 export class FileUploadService {
-  [x: string]: any;
   private url = "http://localhost:9100/collections";
 
+  // Options HTTP pour les requêtes
   httpOptions: { headers: HttpHeaders } = {
     headers: new HttpHeaders({ "Content-Type": "application/json" }),
   };
@@ -25,34 +20,34 @@ export class FileUploadService {
     private http: HttpClient
   ) {}
 
-
-
-  upload(formData: FormData): Observable<any> {
-    return this.http
-      .put<any>(this.url+'/save', formData, this.httpOptions)
-      .pipe(catchError(this.errorHandlerService.handleError<any>("update")));
-  }
-
-  deleteFile(file: string): Observable<any> {
-    const url = this.url+`/delete/${file}`;
-
-    return this.http
-      .delete<any>(url, this.httpOptions)
-      .pipe(catchError(this.errorHandlerService.handleError<any>("delete")));
-  }
+  // Méthode pour télécharger un fichier
   getFile(file: string): Observable<any> {
-    //console.log(id);
-    const url = this.url+`/file/${file}`;
+    const url = `${this.url}/file/${file}`;
     return this.http
       .get<any>(url, { responseType: "json" })
       .pipe(catchError(this.errorHandlerService.handleError<any>("consulter")));
   }
 
+  // Méthode pour télécharger tous les fichiers
   fetchAll(): Observable<any> {
-    const url = this.url+`/all`;
+    const url = `${this.url}/all`;
     return this.http
       .get<any>(url, { responseType: "json" })
       .pipe(catchError(this.errorHandlerService.handleError<any>("all collections")));
   }
 
+  // Méthode pour téléverser un fichier
+  upload(formData: FormData): Observable<any> {
+    return this.http
+      .put<any>(`${this.url}/save`, formData, this.httpOptions)
+      .pipe(catchError(this.errorHandlerService.handleError<any>("update")));
+  }
+
+  // Méthode pour supprimer un fichier
+  deleteFile(file: string): Observable<any> {
+    const url = `${this.url}/delete/${file}`;
+    return this.http
+      .delete<any>(url, this.httpOptions)
+      .pipe(catchError(this.errorHandlerService.handleError<any>("delete")));
+  }
 }
